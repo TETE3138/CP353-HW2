e6c2a303621a64629ec1dac177f9ace2809892f0
TETE <tewichtete31@hotmail.com>
2017-08-31 16:05:34 +0700

    Add files

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,1 +1,15 @@
-"use strict";
+'use strict';
+
+$(".letter").click(function () {
+	$('#write').val($('#write').val() + event.target.innerHTML);
+	$('#write').focus();
+});
+$(".return").click(function () {
+	$('#write').val($('#write').val() + String.fromCharCode(13, 10));
+	$('#write').focus();
+});
+$(".delete").click(function () {
+
+	$('#write').val($('#write').val().slice(0, -1));
+	$('#write').focus();
+});
---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(".letter").click(function(){
+	$('#write').val($('#write').val() + event.target.innerHTML);
+	$('#write').focus();
+});
+$(".return").click(function(){
+	$('#write').val($('#write').val() + String.fromCharCode(13, 10));
+	$('#write').focus();
+});
+$(".delete").click(function(){
+	
+$('#write').val($('#write').val().slice(0,-1));
+		$('#write').focus();
+});
